f5b1c26a705c1e6901259542aec1a353b727c2e0
aryanmishra28 <aryanmishra1851@gmail.com>
2026-03-13 23:42:13 +0530

    Updated program.cs
    Configure JSON serialization to ignore reference cycles
    
    Added AddJsonOptions to controllers setup, setting ReferenceHandler.IgnoreCycles to prevent issues with circular references during JSON serialization in API responses.

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -3,7 +3,11 @@ using RestaurantAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 // Swagger services
 builder.Services.AddEndpointsApiExplorer();
